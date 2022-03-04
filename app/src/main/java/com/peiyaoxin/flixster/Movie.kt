b778498cf2532ec330b33dd3870a8bfff780320a
xelody <melodypxin@gmail.com>
2022-03-03 19:37:01 -0700

    Added placeholder and improved UI

--- app/src/main/java/com/peiyaoxin/flixster/Movie.kt
+++ app/src/main/java/com/peiyaoxin/flixster/Movie.kt
@@ -1,15 +1,23 @@
 package com.peiyaoxin.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
+
+@Parcelize
 
 data class Movie (
     val moveID: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     private val backdropPath: String,
     val title: String,
     val overview: String,
-) {
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    @IgnoredOnParcel
     val backdropImageUrl = "https://image.tmdb.org/t/p/w342/$backdropPath"
 
     companion object {
@@ -20,6 +28,7 @@ data class Movie (
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("backdrop_path"),
                         movieJson.getString("title"),
--- app/src/main/java/com/peiyaoxin/flixster/MovieAdapter.kt
+++ app/src/main/java/com/peiyaoxin/flixster/MovieAdapter.kt
@@ -2,6 +2,7 @@ package com.peiyaoxin.flixster
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -10,11 +11,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -38,29 +41,32 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
         this.orientation = orientation
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-                Glide.with(context)
-                    .load(movie.posterImageUrl)
-                    .placeholder(ColorDrawable(Color.GRAY))
-                    .apply(RequestOptions().override(330, 600))
-                    .transition(DrawableTransitionOptions.withCrossFade())
-                    .into(ivPoster)
+                Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
             } else {
-                Glide.with(context)
-                    .load(movie.backdropImageUrl)
-                    .placeholder(ColorDrawable(Color.GRAY))
-                    .apply(RequestOptions().override(330, 330))
-                    .transition(DrawableTransitionOptions.withCrossFade())
-                    .into(ivPoster)
+                Glide.with(context).load(movie.backdropImageUrl).into(ivPoster)
             }
+        }
+
+        override fun onClick(v: View?) {
+            // 1. Get notified of the particular movie which was clicked
+            val movie = movies[adapterPosition]
+            // 2. Use the intent system to navigate to the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 }
